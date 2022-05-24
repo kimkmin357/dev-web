14124fe4094c5f9ce1c674fd2819fdec7523f0fb
Kyungmin Kim <kimkmin357@gmail.com>
2022-05-24 21:36:20 +0900

    study: nodejs와 react 연동 튜토리얼 - 뭔가 꼬여서 삭제

--- nodejs_react/apis/api.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-router.get('/getDataFromServer', function(req, res){
-    res.send({data:'Server API 1'});
-});
-
-module.exports = router;
